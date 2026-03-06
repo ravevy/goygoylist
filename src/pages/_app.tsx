c597835695aedcb914499a82a8f185992e59138f
beyzabaser <beyzbaser@gmail.com>
2026-03-06 05:37:21 +0300

    feat: wrap app to theme provider and add font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import '@/styles/globals.css'
+import 'nes.css/css/nes.min.css'
+import '@hackernoon/pixel-icon-library/fonts/iconfont.css'
 import type { AppProps } from 'next/app'
 import ProtectedRoute from '@/components/protectedRoute'
+import { ThemeProvider } from '@/context/themeContext'
+import ThemeToggleButton from '@/components/features/ThemeToggleButton'
 
 const publicPages = ['/login']
 
@@ -8,12 +12,18 @@ export default function App({ Component, pageProps, router }: AppProps) {
   const isPublicPage = publicPages.includes(router.pathname)
 
   if (isPublicPage) {
-    return <Component {...pageProps} />
+    return (
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    )
   }
 
   return (
-    <ProtectedRoute>
-      <Component {...pageProps} />
-    </ProtectedRoute>
+    <ThemeProvider>
+      <ProtectedRoute>
+        <Component {...pageProps} />
+      </ProtectedRoute>
+    </ThemeProvider>
   )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Press+Start+2P"
+          rel="stylesheet"
+        />
+        <link href="https://unpkg.com/nes.css/css/nes.css" rel="stylesheet" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
