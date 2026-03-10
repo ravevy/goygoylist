2ff2692256d57e4e7888fad35ce534fdad0a0ff7
beyzabaser <beyzbaser@gmail.com>
2026-03-10 05:39:36 +0300

    feat: add select component

---
+++ src/components/ui-kit/select.tsx
@@ -0,0 +1,86 @@
+import { useTheme } from '@/context/themeContext'
+import React from 'react'
+
+type Variant = 'default' | 'success' | 'warning' | 'error' | 'dark'
+type Size = 'xs' | 'sm' | 'md'
+
+interface Option {
+  label: string
+  value: string | number
+}
+
+interface SelectProps extends Omit<
+  React.SelectHTMLAttributes<HTMLSelectElement>,
+  'size'
+> {
+  label?: string
+  variant?: Variant
+  size?: Size
+  options: Option[]
+}
+
+export const Select = ({
+  label,
+  variant = 'default',
+  size = 'md',
+  options,
+  className = '',
+  ...props
+}: SelectProps) => {
+  const { theme } = useTheme()
+
+  const variantClassMap: Record<Variant, string> = {
+    default: '',
+    success: 'is-success',
+    warning: 'is-warning',
+    error: 'is-error',
+    dark: 'is-dark'
+  }
+
+  const sizeClassMap: Record<Size, string> = {
+    xs: 'text-xs',
+    sm: 'text-sm',
+    md: ''
+  }
+
+  const containerClass = `nes-select ${
+    theme === 'dark'
+      ? `is-dark ${variantClassMap[variant]}`
+      : variantClassMap[variant]
+  } ${className}`
+
+  return (
+    <div
+      className="nes-field"
+      style={
+        variant === 'dark'
+          ? { backgroundColor: '#212529', padding: '1rem' }
+          : undefined
+      }
+    >
+      {label && (
+        <label
+          htmlFor={props.id}
+          className={`nes-text ${variantClassMap[variant]} ${sizeClassMap[size]}`}
+          style={variant === 'dark' ? { color: '#fff' } : undefined}
+        >
+          {label}
+        </label>
+      )}
+
+      <div className={containerClass}>
+        <select className={sizeClassMap[size]} {...props}>
+          <option value="" disabled hidden>
+            Select...
+          </option>
+
+          {options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  )
+}
