4affa65d383299c653a48ad95688a62abf45d48a
beyzabaser <beyzbaser@gmail.com>
2026-03-13 00:30:51 +0300

    feat: add pb to layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -32,7 +32,7 @@ export const Layout = ({ children }: LayoutProps) => {
           <ThemeToggleButton size="m" />
         </Container>
       </nav>
-      <main className="px-4 sm:px-6 md:px-8 lg:px-12 xl:px-16 2xl:px-24">
+      <main className="px-4 pb-10 sm:px-6 md:px-8 lg:px-12 xl:px-16 2xl:px-24">
         {children}
       </main>
     </div>
